b1190d67205079876bac0e7a37a10679d37642f9
The Zenith <abdurrahman.aderinto@gmail.com>
2025-01-02 06:16:53 +0100

    updated

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,22 @@ struct Deck{
 }
 
 fn main() {
+    // List of 'suits' - 'hearts', 'spades'
+    let suits = ["Hearts", "Spades", "Diamonds"];
+    // List of 'values' - 'ace', 'two', 'three'
+    let values = ["Ace", "Two", "Three"];
+
+    let cards = vec![];
+
+    // Double nested loop
+    for suit in suits{
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(value);
+        }
+    }
+
+
     let deck = Deck{cards:vec![]};
     println!("Here's your deck: {:?}", deck);
 }

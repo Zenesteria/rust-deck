509847b8ccc3d53acf7b65e4b0ec9c7df5de60ad
The Zenith <abdurrahman.aderinto@gmail.com>
2025-01-02 06:23:13 +0100

    updated

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,17 @@ fn main() {
     // List of 'values' - 'ace', 'two', 'three'
     let values = ["Ace", "Two", "Three"];
 
-    let cards = vec![];
+    let mut cards = vec![];
 
     // Double nested loop
     for suit in suits{
         for value in values {
             let card = format!("{} of {}", value, suit);
-            cards.push(value);
+            cards.push(card);
         }
     }
 
 
-    let deck = Deck{cards:vec![]};
-    println!("Here's your deck: {:?}", deck);
+    let deck = Deck{cards};
+    println!("Here's your deck: {:#?}", deck);
 }
